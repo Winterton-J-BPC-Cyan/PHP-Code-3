4a1cb73bf8557c6b24121a303318a8d7c1c59faa
Josh winterton <32195114+Winterton-J-BPC-Cyan@users.noreply.github.com>
2017-11-24 09:23:28 +0000

    Initial index page

---
+++ index.php
@@ -0,0 +1,19 @@
+<html>
+
+<body>
+
+<?php
+$number = 1;
+echo "The first number is $number<br>";
+while ($number < 9) {
+    $number = $number + 1;
+    echo "$number <br>";
+}
+if ($number == 9) {
+    echo "Dammit, I'm like a confused 6";
+}
+?>
+
+</body>
+
+</html>
